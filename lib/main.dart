100bf4af8fbce5b3353eb5af8f5d9b41707e91e9
Hand-Tomi <seaud5@gmail.com>
2021-01-03 15:44:13 +0900

    main.dart에 있는 HomePage의 내용을 home_page.dart에 옮기다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 
-final dummyItems = [
-  'https://cdn.pixabay.com/photo/2013/07/25/13/01/stones-167089_1280.jpg',
-  'https://cdn.pixabay.com/photo/2017/09/16/16/09/sea-2755908_1280.jpg',
-  'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_1280.jpg',
-];
+import 'home/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -31,7 +26,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   var _index = 0; // 페이지 인덱스 0, 1, 2
   var _pages = [
-    Page1(),
+    HomePage(),
     Page2(),
     Page3(),
   ];
@@ -71,166 +66,6 @@ class _MyHomePageState extends State<MyHomePage> {
               label: '내 정보', icon: Icon(Icons.account_circle)),
         ],
       ),
-    );
-  }
-}
-
-class Page1 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        _buildTop(),
-        _buildMiddle(),
-        _buildBottom(),
-      ],
-    );
-  }
-
-  // 상단
-  Widget _buildTop() {
-    return Padding(
-      padding: const EdgeInsets.only(top: 20, bottom: 20),
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Column(
-                children: [
-                  Icon(
-                    Icons.local_taxi,
-                    size: 40,
-                  ),
-                  Text('택시'),
-                ],
-              ),
-              Column(
-                children: [
-                  Icon(
-                    Icons.local_taxi,
-                    size: 40,
-                  ),
-                  Text('택시'),
-                ],
-              ),
-              Column(
-                children: [
-                  Icon(
-                    Icons.local_taxi,
-                    size: 40,
-                  ),
-                  Text('택시'),
-                ],
-              ),
-              Column(
-                children: [
-                  Icon(
-                    Icons.local_taxi,
-                    size: 40,
-                  ),
-                  Text('택시'),
-                ],
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Column(
-                children: [
-                  Icon(
-                    Icons.local_taxi,
-                    size: 40,
-                  ),
-                  Text('택시'),
-                ],
-              ),
-              Column(
-                children: [
-                  Icon(
-                    Icons.local_taxi,
-                    size: 40,
-                  ),
-                  Text('택시'),
-                ],
-              ),
-              GestureDetector(
-                onTap: () {
-                  print('클릭');
-                },
-                child: Column(
-                  children: [
-                    Icon(
-                      Icons.local_taxi,
-                      size: 40,
-                    ),
-                    Text('택시'),
-                  ],
-                ),
-              ),
-              Opacity(
-                opacity: 0.0,
-                child: Column(
-                  children: [
-                    Icon(
-                      Icons.local_taxi,
-                      size: 40,
-                    ),
-                    Text('택시'),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-
-  // 중단
-  Widget _buildMiddle() {
-    return CarouselSlider(
-      options: CarouselOptions(
-        height: 150.0,
-        autoPlay: true,
-      ),
-      items: dummyItems.map((url) {
-        return Builder(
-          builder: (BuildContext context) {
-            return Container(
-              width: MediaQuery.of(context).size.width,
-              margin: EdgeInsets.symmetric(horizontal: 5.0),
-              decoration: BoxDecoration(color: Colors.amber),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(8.0),
-                child: Image.network(
-                  url,
-                  fit: BoxFit.cover,
-                ),
-              ),
-            );
-          },
-        );
-      }).toList(),
-    );
-  }
-
-  // 하단
-  Widget _buildBottom() {
-    final items = List.generate(10, (i) {
-      return ListTile(
-        leading: Icon(Icons.notifications_none),
-        title: Text('[이벤트] 이것은 공지사항입니다'),
-      );
-    });
-    return ListView(
-      physics: NeverScrollableScrollPhysics(),
-      shrinkWrap: true,
-      children: items,
     );
   }
 }
